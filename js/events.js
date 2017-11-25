6c44cede6818c93427d078ead8cdc41fcd31db12
Travis Lavery <travisalavery@gmail.com>
2017-11-25 05:15:43 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  $('p').on('click', getIt);
+  $('img').on('load', frameIt);
+  $('form').on('submit', submitIt);
+  pressIt()
 });
+
+function getIt() {
+  alert('Hey!');
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key){
+    if(key.which == 71) {
+      alert('You pressed G!')
+    }
+  });
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.")
+}
