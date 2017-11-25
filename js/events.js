39a6329fb894cecf0519c57068ade3c6233174b2
Travis Lavery <travisalavery@gmail.com>
2017-11-25 04:10:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
+  $('p').on('click', getIt);
+  $('img').on('load', frameIt);
+  $('input').on('keydown', pressIt);
+  $('form').on('submit', submitIt);
 });
+
+function getIt() {
+  alert('Hey!');
+}
+
+function frameIt() {
+  this.addClass('tasty')
+}
+
+function pressIt() {
+  if(this.key == 71) {
+    alert('You pressed G!')
+  }
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.")
+}
